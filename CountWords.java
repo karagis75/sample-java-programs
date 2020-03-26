303e720721550613f475b605b1753bd8df934eec
karagis75 <62547841+karagis75@users.noreply.github.com>
2020-03-26 10:12:43 +0530

    Update CountWords.java

--- CountWords.java
+++ CountWords.java
@@ -1,6 +1,5 @@
-import java.util.HashMap;
 import java.util.Scanner;
-class CountWords{
+class Main{
   public static void main(String[] args) {
         System.out.println(" Please enter the input string :" );
         Scanner in = new Scanner (System.in);
@@ -11,16 +10,17 @@ class CountWords{
 
    public static void  countnoofwords(String str)
     {
-        HashMap<Integer,Character>  characterhashtable= 
-                     new HashMap<Integer,Character>();
+        
         int i,length,j ;
         Character c ;
-        length= str.length();  // Scan string and build hash table
+        length= str.length();   
+
+        //Default set the word count as 1 if length of string is more than 0
         int wordcount = 0; 
         if(length > 0) {
           wordcount = wordcount +1;
         }
-        StringBuffer tmp = new StringBuffer("");
+     
         for (i=0;i < length;i++)
         {  
             c=str.charAt(i);
